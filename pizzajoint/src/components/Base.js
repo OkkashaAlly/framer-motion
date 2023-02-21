6321cfc899d1b228ad16aa57b86cb2e3ffc0f9bf
OkkashaAlly <steamnature006@gmail.com>
2023-02-21 08:03:45 +0300

    SRC: VARIANTS 1

--- pizzajoint/src/components/Base.js
+++ pizzajoint/src/components/Base.js
@@ -2,15 +2,24 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
+const containerVariants = {
+  hidden: { x: "100vw", opacity: 0 },
+  visible: { x: 0, opacity: 1, transition: { delay: 0.5 } },
+};
+const buttonVariants = {
+  hidden: { x: "-100vw", opacity: 0 },
+  visible: { x: 0, opacity: 1, transition: { type: "spring", stiffness: 120 } },
+};
+
 const Base = ({ addBase, pizza }) => {
   const bases = ["Classic", "Thin & Crispy", "Thick Crust"];
 
   return (
     <motion.div
       className="base container"
-      initial={{ x: "100vw" }}
-      animate={{ x: 0 }}
-      transition={{ delay: 0.5 }}
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
     >
       <h3>Step 1: Choose Your Base</h3>
       <ul>
@@ -34,12 +43,7 @@ const Base = ({ addBase, pizza }) => {
       </ul>
 
       {pizza.base && (
-        <motion.div
-          className="next"
-          initial={{ x: "-100vw" }}
-          animate={{ x: 0 }}
-          transition={{ type: "spring", stiffness: 120 }}
-        >
+        <motion.div className="next" variants={buttonVariants}>
           <Link to="/toppings">
             <motion.button
               whileHover={{
