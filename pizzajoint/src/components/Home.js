4bffacd65ab7e46bbd712b62d7d0fe93891efceb
OkkashaAlly <steamnature006@gmail.com>
2023-02-21 07:20:42 +0300

    SRC: INITIAL ANIMATION STATE

--- pizzajoint/src/components/Home.js
+++ pizzajoint/src/components/Home.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import {motion} from 'framer-motion'
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
-    <div className="home container">
-      <motion.h2 animate={{fontSize: 50, color: 'orangered', x: 100, y: -100}}>
-        Welcome to Pizza Joint
-      </motion.h2>
+    <motion.div
+      className="home container"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+    >
+      <h2>Welcome to Pizza Joint</h2>
       <Link to="/base">
-        <motion.button animate={{scale: 1.5}}>
+        <motion.button animate={{ scale: 1.5 }}>
           Create Your Pizza
         </motion.button>
       </Link>
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
 export default Home;
