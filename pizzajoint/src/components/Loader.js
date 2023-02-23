79bee04c027f85eb9da19ced118a8e54e75295b0
OkkashaAlly <steamnature006@gmail.com>
2023-02-23 07:06:48 +0300

    SRC: CYCLE/CHANGE ANIMATION WITH useCycle HOOK

--- pizzajoint/src/components/Loader.js
+++ pizzajoint/src/components/Loader.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { motion } from "framer-motion";
+import { motion, useCycle } from "framer-motion";
 
 const loaderVariants = {
-  visible: {
+  animationOne: {
     x: [-20, 20],
     y: [0, -30],
     transition: {
@@ -10,10 +10,29 @@ const loaderVariants = {
       y: { yoyo: Infinity, duration: 0.25, ease: "easeOut" },
     },
   },
+  animationTwo: {
+    y: [0, -40],
+    x: 0,
+    transition: {
+      y: { yoyo: Infinity, duration: 0.25, ease: "easeOut" },
+    },
+  },
 };
 
-const Loader = () => (
-  <motion.div className="loader" variants={loaderVariants} animate="visible" />
-);
+const Loader = () => {
+  const [animation, cycleAnimation] = useCycle("animationOne", "animationTwo");
+  return (
+    <>
+      <motion.div
+        className="loader"
+        variants={loaderVariants}
+        animate={animation}
+      />
+      <div className="" onClick={() => cycleAnimation()}>
+        cycle animation
+      </div>
+    </>
+  );
+};
 
 export default Loader;
